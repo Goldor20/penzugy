076b75e21a4f8f88715b6489da829e5a3c80f488
KristofUwU <balogh.kristof.janos2009@tanulo.boronkay.hu>
2026-05-24 19:20:21 +0200

    Nemtom

--- penzugy.py
+++ penzugy.py
@@ -1,10 +1,11 @@
-class kerdes():
+import random
+class Kerdes():
     def __init__(self,sorszam,tipus,helyes,szoveg):
         self.sorszam = sorszam
         self.tipus = tipus
         self.helyes = helyes
         self.szoveg = szoveg
-class valasz():
+class Valasz():
     def __init__(self,sorszam,id,valasz):
         self.sorszam = sorszam
         self.id = id
@@ -20,15 +21,22 @@ adat2 = fajl.readline().strip()
 while adat1 != "":
     adat1 = adat1.split(";")
     adat1[0] = int(adat1[0])
-    t.append(kerdes(adat1[0],adat1[1],adat1[2],adat1[3])) 
+    t.append(Kerdes(adat1[0],adat1[1],adat1[2],adat1[3])) 
     adat1 = file.readline().strip()
 
 while adat2 != "":
     adat2 = adat2.split(";")
     adat2[0] = int(adat2[0])
-    t1.append(valasz(adat2[0],adat2[1],adat2[2])) 
+    t1.append(Valasz(adat2[0],adat2[1],adat2[2])) 
     adat2 = file.readline().strip()
 
 be1 = int(input(f"Hány kört szeretne játszani (max: {round(len(t)/2)}): "))
 while be1 > len(t)/2:
     be1 = int(input(f"Hány kört szeretne játszani (max: {round(len(t)/2)}): "))
+szamlalo1 = 0
+szamlalo2 = 0
+
+for i in range(be1):
+    veletlen = random.choice(t)
+    
+
